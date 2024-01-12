2bb9edf503810dbe5d3ec91a3fed8fdbde8256e3
Nilay <nilay.parmar@wickes.co.uk>
2024-01-12 17:52:48 +0530

    init

---
+++ scripts/scripts/setAWSEnvironment.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+echo '*********************************************'
+echo 'creating ~/.aws'
+mkdir -p ~/.aws
+printf "[wickes-sls]\naws_access_key_id=$AWS_KEY\naws_secret_access_key=$AWS_SECRET\n[wickes-sls-test]\nrole_arn=arn:aws:iam::350245524826:role/sts_account_admin\nsource_profile=wickes-sls" > ~/.aws/credentials
+echo '*********************************************'
