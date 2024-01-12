c1498c942313e584d9737e556d045d08229ac5f9
Nilay <nilay.parmar@wickes.co.uk>
2024-01-12 18:38:23 +0530

    update script

--- scripts/setAWSEnvironment.sh
+++ scripts/setAWSEnvironment.sh
@@ -2,5 +2,5 @@
 echo '*********************************************'
 echo 'creating ~/.aws'
 mkdir -p ~/.aws
-printf "[wickes-sls]\naws_access_key_id=$AWS_KEY\naws_secret_access_key=$AWS_SECRET\n[wickes-sls-test]\nrole_arn=arn:aws:iam::350245524826:role/sts_account_admin\nsource_profile=wickes-sls" > ~/.aws/credentials
+printf "[wickes-sls]\naws_access_key_id=\"$AWS_KEY\"\naws_secret_access_key=\"$AWS_SECRET\"\n[wickes-sls-test]\nrole_arn=arn:aws:iam::350245524826:role/sts_account_admin\nsource_profile=wickes-sls" > ~/.aws/credentials
 echo '*********************************************'
